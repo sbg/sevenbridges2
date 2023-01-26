8ea17b130b9627a700d3a357a77c1b12af15e428
Marko Trifunovic <marko.trifunovic@sbgenomics.com>
2023-01-27 00:36:28 +0100

    Remove (starting) dots from variable names.

--- R/sevenbridges2-package.R
+++ R/sevenbridges2-package.R
@@ -1,3 +1,25 @@
+# Seven Bridges API base url
+sbg_baseurl <- list(
+  "cgc" = "https://cgc-api.sbgenomics.com/v2/",
+  "aws-us" = "https://api.sbgenomics.com/v2/",
+  "aws-eu" = "https://eu-api.sbgenomics.com/v2/",
+  "ali-cn" = "https://api.sevenbridges.cn/v2/",
+  "cavatica" = "https://cavatica-api.sbgenomics.com/v2/",
+  "f4c" = "https://api.sb.biodatacatalyst.nhlbi.nih.gov/v2/"
+)
+
+# default platform
+sbg_default_platform <- "cgc"
+
+# default user configuration file
+sbg_default_config_file <- "~/.sevenbridges/credentials"
+sbg_default_profile_name <- "default"
+
+# default system environment variable names
+sbg_default_sysenv_url <- "SB_API_ENDPOINT"
+sbg_default_sysenv_token <- "SB_AUTH_TOKEN"
+
+
 # onLoad function sets initial params for future queries
 .onLoad <- function(libname, pkgname) {
   lst <- list(
@@ -7,5 +29,5 @@
     input_check = TRUE
   )
 
-  options(sevenbridges = lst)
+  options(sevenbridges2 = lst)
 }
