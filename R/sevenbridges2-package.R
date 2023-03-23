225e66bb9d6ed72aa002a84f1f78a41cab556620
Marko Trifunovic <marko.trifunovic@sbgenomics.com>
2023-03-23 18:05:23 +0100

    Change default value for limit parameter to 50 (according to the Public API documentation page).

--- R/sevenbridges2-package.R
+++ R/sevenbridges2-package.R
@@ -28,7 +28,7 @@ utils::globalVariables(c(
 .onLoad <- function(libname, pkgname) {
   lst <- list(
     offset = 0,
-    limit = 100,
+    limit = 50,
     advance_access = FALSE,
     input_check = TRUE
   )
