d2be93f13003b73d0982242d041d8afdba48ab13
Marija Jovanovic <marijajovanovic@Marijas-MacBook-Pro.local>
2023-04-25 15:48:13 +0200

    add Part class and upload_info_part method; add print methods;

--- R/class-upload.R
+++ R/class-upload.R
@@ -12,7 +12,7 @@ Upload <- R6::R6Class(
   # inherit = Item,
   portable = FALSE,
   public = list(
-    #' @field upload_id Upload ID received after upload initialisation.
+    #' @field upload_id Upload ID received after upload initialization.
     upload_id = NULL,
     #' @field path Relative or absolute path to the file on the local disc.
     path = NULL,
@@ -27,12 +27,14 @@ Upload <- R6::R6Class(
     overwrite = NULL,
     #' @field file_size File size.
     file_size = NULL,
-    #' @field base_part_size Size of part in bytes.
-    base_part_size = NULL,
+    #' @field part_size Size of part in bytes.
+    part_size = NULL,
     #' @field part_length Number of parts to upload.
     part_length = NULL,
     #' @field parts List of parts to be uploaded (class Part).
     parts = NULL,
+    #' @field initialized If TRUE, upload has been initialized.
+    initialized = NULL,
     #' @field auth Authentication object.
     auth = NULL,
 
@@ -44,11 +46,13 @@ Upload <- R6::R6Class(
     #' @param overwrite If true will overwrite file on the server.
     #' @param file_size File size.
     #' @param part_size Size of a single part in bytes.
+    #' @param initialized If TRUE, upload has been initialized.
     #' @param auth Authentication object.
     #' @param ... Other arguments.
     initialize = function(path = NA, project = NA, parent = NA,
                           file_name = NA, overwrite = NA, file_size = NA,
-                          part_size = NA, auth = NA, ...) {
+                          part_size = NA, initialized = FALSE, auth = NA,
+                          ...) {
       # Initialize Item class
       # super$initialize(...)
 
@@ -68,22 +72,49 @@ Upload <- R6::R6Class(
 
       self$file_name <- file_name
       self$overwrite <- overwrite
-      self$base_part_size <- part_size
+      self$part_size <- part_size
       self$file_size <- file_size
-      self$part_length <- as.integer(
-        ceiling(self$file_size / self$base_part_size)
+      self$part_length <- ifelse(self$file_size == 0, 1,
+        as.integer(
+          ceiling(self$file_size / self$part_size)
+        )
       )
-      self$parts <- list()
-      self$auth <- auth
-    },
 
+      self$initialized <- initialized
+      self$auth <- auth
+      self$parts <- private$generate_parts(
+        self$file_size, self$part_size, self$part_length, self$auth
+      )
+    },
+    # nocov start
+    #' @description Print method for Upload class.
+    #' @importFrom purrr discard
+    #' @importFrom glue glue
+    #' @importFrom cli cli_h1 cli_li cli_ul cli_end cli_bullets
+    print = function() {
+      x <- as.list(self)
+
+      x <- purrr::discard(x, .p = is.function)
+      x <- purrr::discard(x, .p = is.environment)
+      x <- purrr::discard(x, .p = is.null)
+      x <- purrr::discard(x, .p = is.list)
+
+      string <- glue::glue("{names(x)}: {x}")
+
+      cli::cli_h1("Upload")
+
+      cli::cli_li(string)
+
+      # Close container elements
+      cli::cli_end()
+    }, # nocov end
     #' @description Initialize new upload job.
     #' @param ... Other arguments.
     upload_init = function(...) {
       body <- list(
         "name" = self$file_name,
         "size" = self$file_size,
-        "part_size" = self$base_part_size
+        "part_size" = self$part_size
       )
       if (!is.null(self$project)) {
         body[["project"]] <- self$project$id
@@ -105,10 +136,11 @@ Upload <- R6::R6Class(
 
       self$upload_id <- res$upload_id
 
-      self$base_part_size <- as.integer(res$part_size)
+      self$part_size <- as.integer(res$part_size)
       self$part_length <- as.integer(
-        ceiling(self$file_size / self$base_part_size)
+        ceiling(self$file_size / self$part_size)
       )
+      self$initialized <- TRUE
 
       # nolint start
       rlang::inform(glue::glue("New upload job is initialized with upload_id: {self$upload_id}."))
@@ -136,7 +168,46 @@ Upload <- R6::R6Class(
       )
 
       res <- status_check(res)
-      res # prettier print TODO
+      fields_to_show <- c(
+        "upload_id", "project", "parent",
+        "name", "initiated", "part_size",
+        "parallel_uploads", "uploaded_parts_count"
+      )
+      string <- glue::glue("{fields_to_show}: {res[fields_to_show]}")
+
+      cli::cli_h1("Upload info")
+
+      cli::cli_li(string)
+
+      # Close container elements
+      cli::cli_end()
+    }
+  ),
+  private = list(
+    # Helper method that returns list of objects of class Part
+    generate_parts = function(file_size, part_size, part_length, auth) {
+      checkmate::assert_numeric(file_size, null.ok = FALSE)
+      checkmate::assert_numeric(part_size, null.ok = FALSE)
+      checkmate::assert_numeric(part_length, null.ok = FALSE)
+
+      if (part_length > 1) {
+        last_part_size <- file_size - part_size * (part_length - 1)
+        vector_of_part_sizes <- c(
+          rep(part_size, (part_length - 1)),
+          last_part_size
+        )
+      } else {
+        vector_of_part_sizes <- part_size
+      }
+      part_numbers <- seq_len(part_length)
+      parts <- lapply(part_numbers, function(idx) {
+        Part$new(
+          part_number = idx,
+          part_size = vector_of_part_sizes[idx],
+          auth = auth
+        )
+      })
+      return(parts)
     }
   )
 )
@@ -153,7 +224,7 @@ Upload <- R6::R6Class(
 Part <- R6::R6Class(
   # nolint end
   "Part",
-  inherit = Item,
+  # inherit = Item,
   portable = FALSE,
   public = list(
     #' @field part_number Part number.
@@ -179,6 +250,10 @@ Part <- R6::R6Class(
     report = NULL,
     #' @field etag ETag received after starting a part upload.
     etag = NULL,
+    #' @field auth Authentication object.
+    auth = NULL,
+    #' @field response Response object.
+    response = NULL,
 
     #' @description Create a new Part object.
     #' @param part_number Part number.
@@ -192,22 +267,76 @@ Part <- R6::R6Class(
     #' the HTTP part upload request that should be recognized as success.
     #' @param report Report object.
     #' @param etag ETag received after starting a part upload.
+    #' @param auth Authentication object.
     #' @param ... Other arguments.
     initialize = function(part_number = NA, part_size = NA,
                           url = NA, expires = NA, headers = NA,
                           success_codes = NA, report = NA,
-                          etag = NA, ...) {
+                          etag = NA, auth = NA, ...) {
       # Initialize Item class
-      super$initialize(...)
+      # super$initialize(...)
 
       self$part_number <- part_number
       self$part_size <- part_size
       self$url <- url
-      self$expires <- expires # handle this
+      self$expires <- expires
       self$headers <- headers
       self$success_codes <- success_codes
       self$report <- report
       self$etag <- etag
+      self$auth <- auth
+    },
+    # nocov start
+    #' @description Print method for Part class.
+    #' @importFrom purrr discard
+    #' @importFrom glue glue
+    #' @importFrom cli cli_h1 cli_li cli_ul cli_end cli_bullets
+    print = function() {
+      x <- as.list(self)
+
+      x <- purrr::discard(x, .p = is.function)
+      x <- purrr::discard(x, .p = is.environment)
+      x <- purrr::discard(x, .p = is.null)
+      x <- purrr::discard(x, .p = is.list)
+
+      string <- glue::glue("{names(x)}: {x}")
+
+      cli::cli_h1("Part")
+
+      cli::cli_li(string)
+
+      # Close container elements
+      cli::cli_end()
+    }, # nocov end
+    #' @description Get upload part info
+    #' @param upload_id Upload object's ID part belongs to.
+    #' @param ... Other arguments.
+    upload_info_part = function(upload_id, ...) {
+      checkmate::assert_character(upload_id, null.ok = FALSE)
+
+      res <- sevenbridges2::api(
+        path = paste0(
+          "upload/multipart/",
+          upload_id,
+          "/part/",
+          self$part_number
+        ),
+        method = "GET",
+        token = self$auth$get_token(),
+        base_url = self$auth$url,
+        ...
+      )
+
+      res <- status_check(res)
+
+      self$url <- res$url
+      self$etag <- res$etag
+      self$expires <- res$expires
+      self$success_codes <- res$success_codes
+      self$headers <- res$headers
+      self$report <- res$report
+      self$response <- response(res)
+      self
     }
   )
 )
