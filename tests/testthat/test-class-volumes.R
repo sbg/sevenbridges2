d6009043bed60073b2fc773b8560bc5afcacf78a
Marija Jovanovic <marija.jovanovic@sbgenomics.com>
2023-06-05 16:03:40 +0200

    add volumes class tests so far

---
+++ tests/testthat/test-class-volumes.R
@@ -0,0 +1,23 @@
+test_that("Volumes initialization works", {
+  # Resource object creation works
+  testthat::expect_no_error(Volumes$new(auth = setup_auth_object))
+
+  # Resource object class and methods are set
+  checkmate::assert_r6(
+    setup_volumes_obj,
+    classes = c("Resource", "Volumes"),
+    public = c("URL", "query", "get")
+  )
+})
+
+test_that("Volumes get works", {
+  # Setup test parameters for test
+  test_no_id <- list(id = NULL)
+  test_bad_id <- list(id = 1)
+
+  # Get fails when no id is provided
+  testthat::expect_error(do.call(setup_volumes_obj$get, test_no_id))
+
+  # Get fails when bad id is provided
+  testthat::expect_error(do.call(setup_volumes_obj$get, test_bad_id))
+})
