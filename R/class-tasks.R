dabc2ac8dcc784db1f9b015720d96795dd3be425
Marija Jovanovic <marija.jovanovic@sbgenomics.com>
2023-08-10 19:13:08 +0200

    delete Task$rerun() operation as agreed; add in_place parameters and its handling where needed; add Task$update() method; add batch and batch_by parameters in create_task function (wip)

--- R/class-tasks.R
+++ R/class-tasks.R
@@ -270,6 +270,7 @@ Tasks <- R6::R6Class(
     #' batch. You would typically batch on the input consisting of a list of
     #' files. If this parameter is omitted, the default batching criteria
     #' defined for the app will be used.
+    #' @param batch_by String. Batching criteria.
     #' @param use_interruptible_instances Boolean. This field can be `TRUE` or
     #' `FALSE`. Set this field to `TRUE` to allow the use of
     # nolint start
@@ -288,8 +289,9 @@ Tasks <- R6::R6Class(
                       description = NULL,
                       execution_settings = NULL,
                       inputs = NULL,
-                      batch = NULL,
+                      batch = FALSE,
                       batch_input = NULL,
+                      batch_by = NULL,
                       use_interruptible_instances = NULL,
                       action = NULL,
                       ...) {
@@ -315,8 +317,10 @@ Tasks <- R6::R6Class(
       checkmate::assert_string(name, null.ok = TRUE)
       checkmate::assert_string(description, null.ok = TRUE)
       check_execution_settings(execution_settings)
-      checkmate::assert_logical(batch, null.ok = TRUE)
+      checkmate::assert_list(inputs, null.ok = TRUE)
+      checkmate::assert_logical(batch, null.ok = FALSE)
       checkmate::assert_string(batch_input, null.ok = TRUE)
+      checkmate::assert_string(batch_by, null.ok = TRUE)
       checkmate::assert_logical(use_interruptible_instances, null.ok = TRUE)
       checkmate::assert_string(action, null.ok = TRUE)
 
@@ -324,19 +328,22 @@ Tasks <- R6::R6Class(
       task_data <- list()
       params <- list()
 
-      if (!is_missing(batch_input) && !is_missing(batch_by)) {
-        task_data[["batch_input"]] <- batch_input
-        task_data[["batch_by"]] <- batch_by
-      }
-
       task_meta <- list(
         "name" = name,
         "project" = project_id,
         "app" = app_id,
         "description" = description
       )
-
       task_data <- c(task_data, task_meta)
+
+      if (batch) {
+        if (!is_missing(batch_input) && !is_missing(batch_by)) {
+          task_data[["batch_input"]] <- batch_input
+          # task_data[["batch_by"]] <- batch_by
+        } else {
+          rlang::abort("Batch is set to TRUE, therefore, please, set batching criteria (batch_by) and batch inputs.") # nolint
+        }
+      }
 
       if (!is_missing(inputs)) {
         task_inputs <- private$serialize_inputs(inputs)
@@ -349,6 +356,7 @@ Tasks <- R6::R6Class(
       task_data[["execution_settings"]] <- execution_settings
 
       params[["action"]] <- action
+      params[["batch"]] <- batch
 
       res <- sevenbridges2::api(
         path = self$URL[["query"]],
