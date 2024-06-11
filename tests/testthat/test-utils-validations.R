f6bc03211d2376ffb19c342da5db1c7f02a561c3
Marko Trifunovic <marko.trifunovic@velsera.com>
2024-06-11 12:28:25 +0200

    Fix unit tests for check_and_process_file_details utility function.

--- tests/testthat/test-utils-validations.R
+++ tests/testthat/test-utils-validations.R
@@ -560,35 +560,37 @@ test_that("check_and_transform_datetime works as expected", {
   )
 })
 
+# nolint start
 test_that("check_and_process_file_details throws error for invalid name type", {
-  file_with_wrong_name <- setup_file_obj
+  file_with_wrong_name <- list2env(as.list(setup_file_obj), envir = new.env())
   file_with_wrong_name$name <- 123
 
   testthat::expect_error(
-    do.call(check_and_process_file_details, file_with_wrong_name),
-    regexp = "Assertion on 'file$name' failed: Must be of type 'string', not 'double'.", # nolint
+    check_and_process_file_details(file_with_wrong_name),
+    regexp = "Assertion on 'file$name' failed: Must be of type 'string', not 'double'.",
     fixed = TRUE
   )
 })
 
 test_that("check_and_process_file_details throws error for invalid tags type", {
-  file_with_invalid_tags <- setup_file_obj
+  file_with_invalid_tags <- list2env(as.list(setup_file_obj), envir = new.env())
   file_with_invalid_tags$tags <- "test-tag"
 
   testthat::expect_error(
-    do.call(check_and_process_file_details, file_with_invalid_tags),
-    regexp = "Assertion on 'file$tags' failed: Must be of type 'list', not 'character'.", # nolint
+    check_and_process_file_details(file_with_invalid_tags),
+    regexp = "Assertion on 'file$tags' failed: Must be of type 'list', not 'character'.",
     fixed = TRUE
   )
 })
 
-test_that("check_and_process_file_details throws error for invalid metadata type", { # nolint
-  file_with_invalid_metadata <- setup_file_obj
+test_that("check_and_process_file_details throws error for invalid metadata type", {
+  file_with_invalid_metadata <- list2env(as.list(setup_file_obj), envir = new.env())
   file_with_invalid_metadata$metadata <- c("sample_id", "acdc")
 
   testthat::expect_error(
-    do.call(check_and_process_file_details, file_with_invalid_tags),
-    regexp = "Assertion on 'file$metadata' failed: Must be of type 'list', not 'character'.", # nolint
+    check_and_process_file_details(file_with_invalid_metadata),
+    regexp = "Assertion on 'file$metadata' failed: Must be of type 'list', not 'character'.",
     fixed = TRUE
   )
 })
+# nolint end
